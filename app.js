ff12f855695a0a36363cc0ec1e8d20c46da18422
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2017-04-01 16:49:23 +0200

    st001 Serve static content from public folder

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
 var app = express();
-
 var port = 3000;
 
-app.get('/hello', function(req, res) {
+// Making the content of "/public/" folder available as "localhost:3000/" to serve static files
+app.use('/', express.static(__dirname + '/public/'));
+
+// Mapping "/api/hello" to respond with a function
+app.get('/api/hello', function(req, res) {
 	var response = {hello: "world"};
 	res.json(response);
 });
